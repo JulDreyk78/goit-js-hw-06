0fd418cb23723a18b472eaefbc2e46e985c4fd4c
Yuliia <yulya_moskalenko_95@bigmir.net>
2022-12-18 23:24:35 +0300

    Task-8-1

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,31 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+  console.log(email, password);
+
+  if (email === " " || password === " ") {
+    alert("Заповніть, будь ласка, всі поля");
+    return;
+  }
+
+  const formElement = {
+    email,
+    password,
+  };
+  console.log(formElement);
+  form.reset();
+}
+
+// const formData = new FormData(event.currentTarget);
+
+// formData.forEach((email, password) => {
+// console.log("name", password);
+// console.log("name", email);
+// });
